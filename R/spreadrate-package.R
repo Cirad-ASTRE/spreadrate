e3f4eb52a6212f7dc9b30f016ec63ba52a86d568
Facundo Muñoz <facundo.munoz@cirad.fr>
2019-06-21 10:44:56 +0200

    package-level documentation

---
+++ R/spreadrate-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+#' @details The main functions are \code{\link{sr_obs}} to define
+#' a dataset of observations prepared for spread-rate estimation, and
+#' \code{\link{sr}} for actually performing the estimation.
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
